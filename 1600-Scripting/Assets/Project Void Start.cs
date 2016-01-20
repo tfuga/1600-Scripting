854a3afa94aa84b729d20039f18b6c07d78fcdc4
Tua Fuga <tuafuga@gmail.com>
2016-01-20 09:49:04 -0700

    IfStatements

---
+++ 1600-Scripting/Assets/Project Void Start.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Project : MonoBehaviour 
+{
+	int myInt = 2000;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myInt = MultiplyByTwo(myInt);
+		//Debug.Log (myInt);
+
+	}
+
+	int MultiplyByTwo (int number) 
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ 1600-Scripting/Assets/Scenes/IfStatement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatement : MonoBehaviour {
+
+	//This is the test of my scripts in my branch
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
